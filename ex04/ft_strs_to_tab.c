26499332ceb5ecdbe8c2f11852b460f15a4fe72c
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-24 10:09:29 +0200

    Fix ex04

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 11:40:53 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/20 08:53:48 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/24 10:03:45 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,16 @@ char	*ft_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+
+	dest = malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (!dest)
+		return (NULL);
+	return (ft_strcpy(dest, src));
+}
+
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
 	t_stock_str	*stock_strs;
@@ -45,23 +55,14 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	stock_strs = malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!stock_strs)
 		return (NULL);
-	i = -1;
-	while (++i < ac)
+	i = 0;
+	while (i < ac)
 	{
-		stock_strs[i].str = av[i];
-		stock_strs[i].size = ft_strlen(stock_strs[i].str) + 1;
-		stock_strs[i].copy = malloc(sizeof(char) * stock_strs[i].size);
-		if (!stock_strs[i].copy)
-		{
-			while (--i)
-				free(stock_strs[i].copy);
-			free(stock_strs);
-			return (NULL);
-		}
-		ft_strcpy(stock_strs[i].copy, stock_strs[i].str);
+		stock_strs[i].str = ft_strdup(av[i]);
+		stock_strs[i].size = ft_strlen(av[i]);
+		stock_strs[i].copy = ft_strdup(av[i]);
+		i++;
 	}
 	stock_strs[i].str = 0;
-	stock_strs[i].size = 1;
-	stock_strs[i].copy = 0;
 	return (stock_strs);
 }
